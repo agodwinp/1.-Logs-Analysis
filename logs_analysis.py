ee73afbfbaec6514ab2b034e6179486e7546eab7
Arun Godwin Patel <arungodwinpatel@googlemail.com>
2019-04-01 12:34:20 +0100

    Updated Python code to PEP 8 standards

--- logs_analysis.py
+++ logs_analysis.py
@@ -1,32 +1,38 @@
 #​ !/usr/bin/env python3
-# 
-# # Python application that builds logs analysis report
+# Python application that builds logs analysis report
 
-from logs_analysisdb import get_most_popular_articles, get_most_popular_authors, get_error_prone_days
+from logs_analysisdb import get_most_popular_articles
+from logs_analysisdb import get_most_popular_authors, get_error_prone_days
+
 
 def most_popular_articles():
-  '''Return the top 3 most popular articles'''
-  title = [title for title, num in get_most_popular_articles()]
-  return title
+    '''Return the top 3 most popular articles'''
+    title = [title for title, num in get_most_popular_articles()]
+    return title
+
 
 def most_popular_authors():
-  '''Return the authors in descending order of article views'''
-  authors = [name for name, num in get_most_popular_authors()]
-  return authors
+    '''Return the authors in descending order of article views'''
+    authors = [name for name, num in get_most_popular_authors()]
+    return authors
+
 
 def error_prone_days():
-  '''Return the days that received > 1% of errors for page requests'''
-  error_days = get_error_prone_days()[0]
-  error_days = [i for i in error_days]
-  return error_days
+    '''Return the days that received > 1% of errors for page requests'''
+    error_days = get_error_prone_days()[0]
+    error_days = [i for i in error_days]
+    return error_days
+
 
 if __name__ == '__main__':
-  title = most_popular_articles()
-  print("\n1. The 3 most popular articles of all time are:\n\n", title)
-  print("\n","-"*50)
-  authors = most_popular_authors()
-  print("\n2. The most popular article authors of all time are:\n\n", authors)
-  print("\n","-"*50)
-  error_days = error_prone_days()
-  print("\n3. The days that had > 1% of requests that led to an error are:\n\n", error_days)
-  print("\n","-"*50)
+    title = most_popular_articles()
+    print("\n1. The 3 most popular articles of all time are:\n\n", title)
+    print("\n", "-"*50)
+    authors = most_popular_authors()
+    print("\n2. The most popular article authors of all time are:\n\n",
+          authors)
+    print("\n", "-"*50)
+    error_days = error_prone_days()
+    print("\n3. Days that had > 1% of requests leading to an error are:\n\n",
+          error_days)
+    print("\n", "-"*50)
--- logs_analysisdb.py
+++ logs_analysisdb.py
@@ -1,37 +1,42 @@
 #​ !/usr/bin/env python3
-# 
-# # Database code for the Project 1 "news" DB
+# Database code for the Project 1 "news" DB
 
 import psycopg2
-
 DBNAME = "news"
 
+
 def get_most_popular_articles():
-  '''Return top 3 most popular articles of all time'''
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  query = "select articles.title, count(*) as num from log_slug join articles on articles.slug = log_slug.slug where method='GET' and status='200 OK' group by articles.title order by num desc limit 3;"
-  c.execute(query)
-  top_3 = c.fetchall()
-  db.close()
-  return top_3
+    '''Return top 3 most popular articles of all time'''
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    query_pt1 = "select articles.title, count(*) as num from log_slug"
+    query_pt2 = " join articles on articles.slug = log_slug.slug where "
+    query_pt3 = " method='GET' and status='200 OK' group by articles.title"
+    query_pt4 = " order by num desc limit 3;"
+    query = query_pt1+query_pt2+query_pt3+query_pt4
+    c.execute(query)
+    top_3 = c.fetchall()
+    db.close()
+    return top_3
+
 
 def get_most_popular_authors():
-  '''Return authors in order of decreasing article views'''
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  query = "select * from author_views"
-  c.execute(query)
-  authors = c.fetchall()
-  db.close()
-  return authors
+    '''Return authors in order of decreasing article views'''
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    query = "select * from author_views"
+    c.execute(query)
+    authors = c.fetchall()
+    db.close()
+    return authors
+
 
 def get_error_prone_days():
-  '''Return the days that have had > 1% of requests leading to errors'''
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  query = "select date::text from log_daily_pct_error where pct_error > 1;"
-  c.execute(query)
-  days = c.fetchall()
-  db.close()
-  return days
+    '''Return the days that have had > 1% of requests leading to errors'''
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    query = "select date::text from log_daily_pct_error where pct_error > 1;"
+    c.execute(query)
+    days = c.fetchall()
+    db.close()
+    return days
